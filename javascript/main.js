0519259b9db106893c0032c2eff97db1a9e3493e
KaiqueFj <kaiquelferraz@hotmail.com>
2022-06-23 06:27:27 -0300

    feat: added new responsive nav into home page, with design for mobile view

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,44 @@
-const btnMobile = document.getElementById('btn-mobile')
-
-function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault()
-    const nav = document.getElementById('nav')
-    nav.classList.toggle('active');
-}
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-nav",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
